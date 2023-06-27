992d2ddb3ae26b0a3af420a349961fed674cfcf3
kgrymd <kgroymd@gmail.com>
2023-06-27 23:53:24 +0900

    APIたたいてみた

--- api/main.py
+++ api/main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
+
+
+# CORSミドルウェアの設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 全てのオリジンからのアクセスを許可します。実際の運用環境では、必要なオリジンのみを許可するよう設定してください。
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
